2f72818f68272446efbf450b3e0f24bbcbeae729
Ifeoma Goody <goodnessezeokoli3@gmail.com>
2025-07-13 18:18:17 -0700

    the file were added

--- app/utils/api.ts
+++ app/utils/api.ts
@@ -1,10 +1,6 @@
 
 export async function signupUser(userData: {
-    email: string;
-    first_name: string;
-    last_name: string;
-    password: string;
-    confirm_password: string;
+ 
 }) {
 
     try{
