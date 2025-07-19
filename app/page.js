1402761216b6800c90188ddf15112b572eb54c58
Ifeoma Goody <goodnessezeokoli3@gmail.com>
2025-07-18 23:33:05 -0700

    the root file was updated

--- app/page.js
+++ app/page.js
@@ -1,8 +1,20 @@
 
+"use client"
 import Image from "next/image";
+import {useRouter} from "next/navigation";
 
+
+//Our Home Page is supposed to be here
 export default function Home() {
+  const router = useRouter()
   return (
-    <h1>Hello</h1>
+<div className="flex items-center justify-center h-screen">
+  <button
+    className="flex border text-center justify-center px-4 py-2"
+    onClick={() => router.push("./signup")}
+  >
+    Signup
+  </button>
+</div>
   );
 }
